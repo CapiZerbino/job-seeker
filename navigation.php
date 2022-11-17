e03a5e3e06e82ffe81f14f1c9eef303c2b560177
Nghia Tieu <92912169+CapiZerbino@users.noreply.github.com>
2022-11-17 12:36:04 +0700

    First commit

---
+++ navigation.php
@@ -0,0 +1,11 @@
+<header>
+    <nav class="navbar bg-light">
+        <div class="container-fluid ">
+            <a class="navbar-brand">JobSeeker</a>
+            <div class="d-flex">
+                <button class="btn btn-outline-success mx-1" type="button">Log in</button>
+                <button class="btn btn-outline-success" type="button">Log out</button>
+            </div>
+        </div>
+    </nav>
+</header>
